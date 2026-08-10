a49acecaaa306f846e3f5978cd60d67b575ba5ec
savrdhcapital-debug <savrdhcapital@gmail.com>
2026-08-10 09:20:18 +0530

    Remove legacy animation files

--- home-animation-init.js
+++
@@ -1,5 +0,0 @@
-(function(){
-  var s=document.createElement('link');s.rel='stylesheet';s.href='home-animations.css';document.head.appendChild(s);
-  var j=document.createElement('script');j.src='home-animations.js';j.defer=true;document.head.appendChild(j);
-  var k=document.createElement('script');k.src='home-animation-loader.js';k.defer=true;document.head.appendChild(k);
-})();
